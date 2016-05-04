8ad24ec1737401a854989262a7a298cbbc79bb9c
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-05-04 12:41:34 +0200

    Added a new class

---
+++ app/src/main/java/com/cogn/wifirecord/PreviousRecordings.java
@@ -0,0 +1,8 @@
+package com.cogn.wifirecord;
+
+/**
+ * Will obtain the coordinates of previous loggings and the details of the summaries for a given
+ * location.
+ */
+public class PreviousRecordings {
+}
